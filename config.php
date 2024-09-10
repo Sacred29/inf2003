c59258ec7cdc5c161970b9dcd6bbeff323cb3400
Daniel <70926662+Sacred29@users.noreply.github.com>
2024-09-10 12:09:37 +0800

    account for vm envvars

--- config.php
+++ config.php
@@ -2,7 +2,13 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Load the .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+if (file_exists(__DIR__ . '/.env')) {
+    // If .env exists, use it for local development
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    echo "Loaded environment variables from .env file (Local environment)";
+}
+// // Load the .env file
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 ?>
