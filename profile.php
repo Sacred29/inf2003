45bb6ae21c780815847c9131c08fba307a3fd40c
ShiLong-Phan <phantasmlied@gmail.com>
2024-09-15 21:53:05 +0800

    added empty profile.php file with just the navbar

---
+++ profile.php
@@ -0,0 +1,27 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="en">
+<!--?php include 'includes/head.php'; ?> -->
+
+<head>
+    <title>Borrowed Book List</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/gallery.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <?php 
+    include 'inc/nav.php';
+    require_once __DIR__ . '/config.php';
+    ?>
+
+
+
+
+</body>
